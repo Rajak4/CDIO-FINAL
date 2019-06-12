2aa761cea7a5d57702eb4206cb200b902717c5ab
Patrick <patrick.lopdrup@gmail.com>
2019-06-12 13:23:01 +0200

    lagt ting over fra gammelt projekt

--- webapp/script.js
+++ webapp/script.js
@@ -3,11 +3,10 @@ $(function () {
     var categories = ["c++", "java", "php", "coldfusion", "javascript", "asp", "ruby"];
     $("#category").autocomplete({
         source: categories,
-        minLenght: 0
+        minLenght: 0,
+        autoFocus: true
     }).focus(function () {
         $(this).autocomplete("Search", "");
     })
 });
-
-
 
