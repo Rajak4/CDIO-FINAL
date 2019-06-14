b198fe084564595c77d7ef6b40468d0c57737b43
Martin Rylund <martin@rylund.eu>
2019-06-14 11:54:23 +0200

    Nyt design til formen

--- target/CDIO-FINAL-1.0-SNAPSHOT/script.js
+++ target/CDIO-FINAL-1.0-SNAPSHOT/script.js
@@ -1,3 +1,5 @@
+//should add <option> tags with values from the array.
+//This way there can only be searched in those specific words.
 var items = ["hej", "med", "dig", "4", "5"];
 $.each(items, function (i, item) {
     console.log(i + " : " + item);
