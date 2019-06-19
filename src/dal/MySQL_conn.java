7b0fd2bcb11a995ea3ee432098b2b96d250ab75c
Martin Rylund <martin@rylund.eu>
2019-06-19 15:24:42 +0200

    Mulig fiks til database fejl

--- src/dal/MySQL_conn.java
+++ src/dal/MySQL_conn.java
@@ -4,25 +4,40 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
+import static java.lang.System.exit;
+
 
 public class MySQL_conn {
     private static Connection connection = null;
     public static Connection getConnection() throws SQLException {
-        try {
-            if (connection != null && !connection.isClosed()) {
-                return connection;
-            } else {
+        int attempts = 0;
+        while(true) {
+            try {
+                if (connection != null && !connection.isClosed()) {
+                    return connection;
+                } else {
                     Class.forName("com.mysql.cj.jdbc.Driver");
 
-                connection = DriverManager.getConnection("jdbc:mysql://sql223.main-hosting.eu/u748359586_02324?"
+                    connection = DriverManager.getConnection("jdbc:mysql://sql223.main-hosting.eu/u748359586_02324?"
                             + "user=u748359586_02324&password=jR2qSR2gHsCK");
                     return connection;
 
+                }
+            } catch (ClassNotFoundException e) {
+                e.printStackTrace();
             }
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
+
+            attempts++;
+            if (attempts > 20) {
+                System.out.println("Database forbindelse fejlede!");
+                exit(1);
+            }
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
         }
-        return null;
     }
 
     public static class DALException extends Exception {
